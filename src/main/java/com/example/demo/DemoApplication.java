3478e34ce2bda4f5f9d0d667319a7e03725a3354
shannu <gundala,shanmukha@gmail.com>
2021-06-21 15:26:04 +0530

    Adding docker file

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 
- @SpringBootApplication
+@SpringBootApplication
     public class DemoApplication extends SpringBootServletInitializer {
 
         public static void main(String[] args) {
